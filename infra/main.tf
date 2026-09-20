fc04b61d33235fd4c37fc55663338d3d75260245
Fady Mounir Zaghloul <133204319+fadyy2k@users.noreply.github.com>
2026-09-20 13:28:39 +0300

    chore: add infra/main.tf

---
+++ infra/main.tf
@@ -0,0 +1,70 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  name = "${var.project_name}-${var.environment}"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = var.vpc_cidr
+
+  azs = local.azs
+
+  private_subnets = [
+    cidrsubnet(var.vpc_cidr, 4, 0),
+    cidrsubnet(var.vpc_cidr, 4, 1)
+  ]
+
+  public_subnets = [
+    cidrsubnet(var.vpc_cidr, 8, 32),
+    cidrsubnet(var.vpc_cidr, 8, 33)
+  ]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = local.name
+  cluster_version = var.kubernetes_version
+
+  cluster_endpoint_public_access  = var.cluster_endpoint_public_access
+  cluster_endpoint_private_access = true
+
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    platform = {
+      instance_types = var.node_instance_types
+
+      min_size     = var.node_min_size
+      max_size     = var.node_max_size
+      desired_size = var.node_desired_size
+
+      capacity_type = "ON_DEMAND"
+    }
+  }
+}
