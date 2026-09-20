1db2d2cb017f56dad446e15de2fbec4cd79724a2
Fady Mounir Zaghloul <133204319+fadyy2k@users.noreply.github.com>
2026-09-20 13:28:41 +0300

    chore: add infra/outputs.tf

---
+++ infra/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  description = "EKS cluster name."
+  value       = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  description = "EKS API endpoint."
+  value       = module.eks.cluster_endpoint
+}
+
+output "vpc_id" {
+  description = "VPC ID."
+  value       = module.vpc.vpc_id
+}
+
+output "private_subnet_ids" {
+  description = "Private subnet IDs used by EKS."
+  value       = module.vpc.private_subnets
+}
